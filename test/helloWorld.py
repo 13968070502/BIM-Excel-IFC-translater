467d6f29408412b3cec12fcd7b70a6090f93cc89
alexwelle <alexanderwellenhofer@gmail.com>
2020-04-08 17:34:24 +0200

    Imported Pandas, selected path, added data.

--- test/helloWorld.py
+++ test/helloWorld.py
@@ -1,1 +1,13 @@
 print ("hello world")
+
+import pandas as pd
+import os
+
+path = 'C:\\Masterarbeit\\Programmieren\\Dateien'
+os.chdir(path)
+
+pokemondata = path+'\\pokemon_data.csv'
+
+df = pd.read_csv(pokemondata)
+
+print df
