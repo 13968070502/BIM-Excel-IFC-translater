cfed00729a0eebd9f80644db2c2055a1d1e61213
alexwelle <alexanderwellenhofer@gmail.com>
2020-04-22 13:13:09 +0200

    Create class: "ifcwall" with inherited abilities.

--- ifc_classes.py
+++ ifc_classes.py
@@ -7,7 +7,7 @@ class ifcroot:
         self.globalid = globalid
 
     def printifcrootabilities(self):
-        print('The ifc-class has the following abilities:')
+        print('The class "ifcroot" has the following abilities:')
         print('GlobalID: ', self.globalid)
 
 
@@ -18,7 +18,7 @@ class ifcobjectdefinition(ifcroot):
         super(ifcobjectdefinition, self).__init__(globalid)
 
     def printifcobjectdefinitionabilities(self):
-        print('The ifc-class has the following abilities:')
+        print('The class "ifcobjectdefinition" has the following abilities:')
         print('GlobalID: ', self.globalid)
 
 
@@ -37,7 +37,7 @@ class ifcobject(ifcobjectdefinition):
         self.istypedby = istypedby
 
     def printifcobjectabilities(self):
-        print('The ifc-class has the following abilities:')
+        print('The class "ifcobject" has the following abilities:')
         print('GlobalID: ', self.globalid)
         print('Name: ', self.name)
         print('Description: ', self.description)
@@ -52,7 +52,7 @@ class ifcproduct(ifcobject):
         super(ifcproduct, self).__init__(globalid, name, description, objecttype, istypedby)
 
     def printifcproductabilities(self):
-        print('The ifc-class has the following abilities:')
+        print('The class "ifcproduct" has the following abilities:')
         print('GlobalID: ', self.globalid)
         print('Name: ', self.name)
         print('Description: ', self.description)
@@ -71,7 +71,7 @@ class ifcelement(ifcproduct):
         self.hasopenings = hasopenings
 
     def printifcelementabilities(self):
-        print('The ifc-class has the following abilities:')
+        print('The class "ifcelement" has the following abilities:')
         print('GlobalID: ', self.globalid)
         print('Name: ', self.name)
         print('Description: ', self.description)
@@ -81,5 +81,48 @@ class ifcelement(ifcproduct):
         print('HasOpenings: ', self.hasopenings)
 
 
-dataifcelement = ifcelement('1234567890', 'Wall', 'vertical construction', 'Component', 'ifcproduct', '0.0.0', 'Yes')
-dataifcelement.printifcelementabilities()
+
+class ifcbuildingelement(ifcelement):
+
+    def __init__(self, globalid, name, description, objecttype, istypedby, objectplacement, hasopenings,):
+        super(ifcbuildingelement, self).__init__(globalid, name, description, objecttype, istypedby, objectplacement, hasopenings)
+
+    def printifcbuildingelementabilities(self):
+        print('The class "ifcbuildingelement" has the following abilities:')
+        print('GlobalID: ', self.globalid)
+        print('Name: ', self.name)
+        print('Description: ', self.description)
+        print('ObjectType: ', self.objecttype)
+        print('IsTypedBy: ', self.istypedby)
+        print('ObjectPlacement: ', self.objectplacement)
+        print('HasOpenings: ', self.hasopenings)
+
+
+
+class ifcwall(ifcbuildingelement):
+    length = ""
+    width = ""
+    height = ""
+
+    def __init__(self, globalid, name, description, objecttype, istypedby, objectplacement, hasopenings, length, width, height):
+        super(ifcwall, self).__init__(globalid, name, description, objecttype, istypedby, objectplacement, hasopenings)
+        self.length = length
+        self.width = width
+        self.height = height
+
+    def printifcwallabilities(self):
+        print('The class "ifcwall" has the following abilities:')
+        print('GlobalID: ', self.globalid)
+        print('Name: ', self.name)
+        print('Description: ', self.description)
+        print('ObjectType: ', self.objecttype)
+        print('IsTypedBy: ', self.istypedby)
+        print('ObjectPlacement: ', self.objectplacement)
+        print('HasOpenings: ', self.hasopenings)
+        print('Length: ', self.length)
+        print('Width: ', self.width)
+        print('height: ', self.height)
+
+dataifcwall = ifcwall('1234567890', 'Wall', 'vertical construction', 'Component', 'ifcbuildingelement', '0.0.0', 'Yes', '5.0', '0.2', '3.0')
+dataifcwall.printifcwallabilities()
+
