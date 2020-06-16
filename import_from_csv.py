e3d8ed9aab2ac465672e6c1a44933a179d672291
alexwelle <alexanderwellenhofer@gmail.com>
2020-06-16 14:33:49 +0200

    test main function

--- import_from_csv.py
+++ import_from_csv.py
@@ -1,23 +1,60 @@
 """A file to import csv files."""
-
 import csv
-
+import itertools
+from openpyxl import load_workbook
+from operator import itemgetter
+import tabulate
 
 path = "data\Input_Results_TGA_MA.csv"
 file = open(path, newline= '')
 reader = csv.reader(file, delimiter=';')
 
-header = next(reader) # The first line is the header
+#header = next(reader) # The first line is the header
 data = [row for row in reader] # Read the remaining data
 
-sort = sorted(reader, key=operator.itemgetter(0))
+def get_only(attribute, data):
+    return [x for x in data if x['IFC_Element'] == attribute]
+
+def show_data(data):
+    """Show content of data object."""
+    groups = itertools.groupby(data, key=itemgetter('IFC_Element'))
+    attributes = [name for name, _ in groups]
+    for attribute in attributes:
+        dataset = get_only(attribute, data)
+        header = dataset[0].keys()
+        rows = [x.values() for x in dataset]
+        print(f'{attribute}:')
+        print(tabulate.tabulate(rows, header, tablefmt='grid'))
 
 
 
+def import_from_csv(filename):
+    """Read a csv file."""
+    if not filename:
+        filename = 'data/Input_Results_TGA_MA.csv'
 
+    print(f'Import file from csv: {filename} ...')
 
+    wb = load_workbook(filename)
 
+    data = []  # list of dictionaries to catch all data
 
+    for ws in wb.worksheets:  # loop over worksheets
+        header_row = next(reader(1, 1), ws)
+        print(f'Read sheet {ws.title}:')
+        print(f'header: {header_row}')
+        row_count = ws.max_row
+        for row_idx in range(1, row_count):  # loop over rows
+            cell_value = ws.cell(row=row_idx+1, column=1).value
+            if not cell_value:
+                continue  # skip if no data in first cell of row
+            this_row_data = {}  # dictionary to catch single row content
+            for col_idx, header in enumerate(header_row):  # loop over columns
+                cell_value = ws.cell(row=row_idx+1, column=col_idx+1).value
+                this_row_data[header] = cell_value
+            data.append(this_row_data)
+
+    show_data(data)
 
 
 
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 """The main file for the program."""
+import import_from_excel
 import import_from_csv
 import translate_to_ifc
 import write_to_step
 
 
-def main():
-    """Run the main functions"""
-    data = import_from_csv.import_from_csv('data\Input_Results_TGA_MA.csv')
-    translate_to_ifc.translate_to_ifc(data)
-    write_to_step.write_to_step(data)
-
 #def main():
-#    """Run the main functions."""
-#    data = import_from_excel.import_from_excel('data/ExcelVorlage.xlsx')
-#    write_to_ifc.write_to_ifc(data)
+#    """Run the main functions"""
+#    data = import_from_csv.import_from_csv('data\Input_Results_TGA_MA.csv')
+#    translate_to_ifc.translate_to_ifc(data)
+#    write_to_step.write_to_step(data)
+
+def main():
+    """Run the main functions."""
+    data = import_from_excel.import_from_excel('data/ExcelVorlage.xlsx')
+ #   translate_to_ifc.translate_to_ifc(data)
 
 if __name__ == '__main__':
     main()
