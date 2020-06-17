5c7b90a7c0fdbc876bbc7b74f39630537b95a1a1
alexwelle <alexanderwellenhofer@gmail.com>
2020-06-17 19:06:16 +0200

    import csv

--- import_from_csv.py
+++ import_from_csv.py
@@ -1,44 +1,70 @@
 """A file to import and write csv files."""
 # Import csv file
+
 import csv
+path = 'data\Input_Results_TGA_MASTER.csv'
+num = 0
+array = []
+
+with open(path, 'r', newline='') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = csv.reader(read_obj, delimiter=';')
+    # Pass reader object to list() to get a list of lists
+    list_of_rows = list(csv_reader)
+
+for s in list_of_rows:
+    print(*s)
+
+for item in list_of_rows[0]:
+    print (item)
+    array.append([item + '=' + str(num)])
+    num += 1
+
+print (array)
+
+Object=0; Object_Id=1; IFC_Element=2; Diameter=3
+
+print (list_of_rows[3][1])
 
 
-Konverted_Input_Results_TGA_MA = []
-path = 'data\Input_Results_TGA_MA.csv'
-file = open(path, newline='')
 
-# Read csv file
-with open(path, 'r') as Input_Results_TGA_MA:
-    csv_dict_reader = csv.DictReader(file, delimiter=';')
 
-   for row in csv_dict_reader:
+
+
+
+
+#--------------------------------------------------------
+
+#path = 'data\Input_Results_TGA_MASTER.csv'
+#file = open(path, newline='')
+#
+## Read csv file
+#with open(path, 'r') as Input_Results_TGA_MA:
+#    csv_dict_reader = csv.DictReader(file, delimiter=';')
+#    header = next(csv_dict_reader) # The first line is the header
+#
+#    for row in csv_dict_reader:
 #        print(row)
-        Konverted_Input_Results_TGA_MA.append({
-            'Object': row['Object'],
-            'Object_Id': row['Object_Id'],
-            'IFC_Element': row['IFC_Element']
-        })
+#        print(header)
 
 
-# Write new csv file
-
-newpath = 'data\Konverted_Input_Results_TGA_MA.csv'
-
-with open('data\Konverted_Input_Results_TGA_MA.csv', 'w', newline='') as Konverted_Input_Results_TGA_MA:
-
-    fieldnames = ['Object', 'Object_Id', 'IFC_Element']
-    csv_dict_writer = csv.DictWriter(Konverted_Input_Results_TGA_MA, fieldnames=fieldnames)
-    csv_dict_writer.writeheader()
-
-    for fieldnames in Konverted_Input_Results_TGA_MA:
-        csv_dict_writer.writerow(fieldnames)
-
-
-
-
+#------------------------------------------------------------
+#
+## Write new csv file
+#
+#newpath = 'data\Konverted_Input_Results_TGA_MA.csv'
+#
+#with open('data\Konverted_Input_Results_TGA_MA.csv', 'w', newline='') as Konverted_Input_Results_TGA_MA:
+#
+#    fieldnames = ['Object', 'Object_Id', 'IFC_Element']
+#    csv_dict_writer = csv.DictWriter(Konverted_Input_Results_TGA_MA, fieldnames=fieldnames)
+#    csv_dict_writer.writeheader()
+#
+#    for fieldnames in Konverted_Input_Results_TGA_MA:
+#        csv_dict_writer.writerow(fieldnames)
 
 
-
+#---------------------------------------------------------------------------
 
 
 #    header = next(csv_dict_reader)  # The first line is the header
