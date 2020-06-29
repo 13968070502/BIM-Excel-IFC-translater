1a41cc8116ca602b63c646d30cd556adc66c7055
alexwelle <alexanderwellenhofer@gmail.com>
2020-06-29 14:06:34 +0200

    update write_to_step.py

--- write_to_step.py
+++ write_to_step.py
@@ -1,114 +1,189 @@
-"""Contains the functionality to write the data out in step format."""
-
+import uuid
+import time
+import tempfile
+import ifcopenshell
 from import_from_csv import get_value
-from create_timestamp import create_timestamp
+
+# Creation of Global Unique Identifier
+create_guid = lambda: ifcopenshell.guid.compress(uuid.uuid1().hex)
 
 
-def write_to_step(data):
-    """Write data in step format."""
+# Direction points to define IfcAxis2Placement
+O = 0., 0., 0.
+X = 1., 0., 0.
+Y = 0., 1., 0.
+Z = 0., 0., 1.
 
 
-# Construction elements of STEP-file
-iso = "ISO-10303-21;"
-header = "HEADER;"
-file_description = "FILE_DESCRIPTION ("
-end_row = ");"
-start_argument = "('"
-end_argument = "'), "
-next_row = '\n'
-file_name = "FILE_NAME ("
-file_schema = "FILE_SCHEMA (('"
-endsec = "ENDSEC;"
-data = "DATA;"
-end_iso = "END-ISO-10303-21;"
-timestamp = create_timestamp()
-representation_type = "('ViewDefinition [DesignTransferView_V1]'), "
-author = "Wellenhofer"
-ifc_version = "IFC4"
-implementation_level = "2;1"
-no_value = "''"
-no_value_b = "('')"
-no_value_S = "$"
-comma = ", "
-apostrophe = "'"
+# Useful functions to generate geometry
 
-# Imported Data from CSV-File
+# Curve
+def create_ifccurve(ifcfile, axis2placement2D, radius):
+    ifccurve = ifcfile.createIfcCurve(axis2placement2D, radius)
+    return ifccurve
 
 
-# Construction of STEP-File
-with open('STEP-file.ifc', 'w') as f:
-    # HEADER section
-    f.write(iso + next_row)
-    f.write(header + next_row)
-    f.write(
-        file_description + representation_type + apostrophe + implementation_level + apostrophe + end_row + next_row)
-    f.write(
-        file_name + no_value + comma + apostrophe + timestamp + apostrophe + comma + start_argument + author + end_argument + no_value_b + comma + no_value + comma + no_value + comma + no_value + end_row + next_row)
-    f.write(file_schema + ifc_version + "')" + end_row + '\n')
-    f.write(endsec + '\n')
-
-    # DATA section
-    f.write(data + '\n')
-
-    # Placement with local coordinates:
-    f.write("#101=IFCCARTESIANPOINT((0.000,0.000,0.000));" + next_row)  # origin point
-    f.write("#102=IFCDIRECTION((1.000,0.000,0.000));" + next_row)  # x-axis
-    f.write("#103=IFCDIRECTION((0.000,1.000,0.000));" + next_row)  # y-axis
-    f.write("#104=IFCDIRECTION((0.000,0.000,1.000));" + next_row)  # z-axis
-    f.write("#105=IFCAXIS2PLACEMENT3D(#101,#104,#102);" + next_row)  # cartesian coordinate system
-
-    # Declaration of Units:
-    f.write("#121=IFCSIUNIT($,.LENGTHUNIT.,$,.METRE.);" + next_row)  # cartesian point coordinates
-    f.write("#122=IFCSIUNIT($,.PLANEANGLEUNIT.,$,.RADIAN.);" + next_row)  # rotation angles
-    f.write("#123=IFCUNITASSIGNMENT((#121,#122));" + next_row)
-
-    # Geometric representations:
-    f.write("#142=IFCGEOMETRICREPRESENTATIONCONTEXT('3D body','3D-Models VR',3,$,#105,$);" + next_row)
-    f.write("#145=IFCGEOMETRICREPRESENTATIONSUBCONTEXT('body300','LOD300',$,$,$,$,#142,$,.MODEL_VIEW.,$);" + next_row)
-
-    # Assign contexts, units to project:
-    f.write("#181=IFCPROJECT('2Dx3oA_r5F69i0DCABvXDv',$,'pipe project',$,$,$,$,(#145),#123);" + next_row)
-
-    # 3D-Body-context:
-    f.write("#201=IFCCARTESIANPOINT((0.000,0.300));" + next_row)  # x=0           , y=inner radius
-    f.write("#202=IFCCARTESIANPOINT((0.000,0.400));" + next_row)  # x=0           , y=outer radius
-    f.write("#203=IFCCARTESIANPOINT((5.000,0.400));" + next_row)  # x=length      , y=outer radius
-    f.write("#204=IFCCARTESIANPOINT((5.000,0.300));" + next_row)  # x=length      , y=inner radius
-    f.write("#205=IFCCARTESIANPOINT((0.000,0.300));" + next_row)
-    f.write("#212=IFCPOLYLINE((#201,#202,#203,#204,#205));" + next_row)
-    f.write("#213=IFCARBITRARYCLOSEDPROFILEDEF(.AREA.,$,#212);" + next_row)  # declare poly as area
-    f.write("#214=IFCAXIS2PLACEMENT3D(#101,#104,#102);" + next_row)  # revolve-plane
-    f.write("#215=IFCAXIS1PLACEMENT(#101,#102);" + next_row)  # rotation axis
-    f.write("#216=IFCREVOLVEDAREASOLID(#213,#214,#215,6.283);" + next_row)  # how
-    f.write("#217=IFCSHAPEREPRESENTATION(#145,'body300',$,(#216));" + next_row)  # as
-
-    # product shape definition, placement and instancing:
-    f.write("#501=IFCPRODUCTDEFINITIONSHAPE($,$,(#217));" + next_row)  # with
-    f.write("#502=IFCLOCALPLACEMENT($,#105);" + next_row)  # where
-    f.write("#503=IFCFLOWSEGMENT('3poERwXYL9lgTG8ACKJuNz',$,'pipe',$,$,#502,#501,$);" + next_row)  # what
-
-    # Assign properties to pipe:
-    f.write("#701=IFCPROPERTYSINGLEVALUE('DN',$,IFCREAL(400),$);" + next_row)
-    f.write("#702=IFCPROPERTYSET('1FGilBX2j3hgY6Jx3rLKfz',$,'pipe-properties',$,(#701));" + next_row)
-    f.write("#703=IFCRELDEFINESBYPROPERTIES('3GIJzeSVL6_x3rDzCQWlth',$,$,$,(#503),#702);" + next_row)
-
-    # Assign elements to project:
-    f.write("#901=IFCRELAGGREGATES('1l4J2aaDD0b9lNurr4uB78',$,$,$,#181,(#503));" + next_row)
-
-    # End file
-    f.write(endsec + next_row)
-    f.write(end_iso + next_row)
-
-step_file = ""
-
-# Read lines in text file
-with open('STEP-file.ifc', 'r') as f:
-    for line in f:
-        print(line)
-
-# Read one line in text file
-# with open('STEP-file.ifc', 'r') as f:
-#    step_file += f.readline()
+# Circle
+def create_ifccircle(ifcfile, axis2placement2D, radius):
+    ifccircle = ifcfile.createIfcCircle(axis2placement2D, radius)
+    return ifccircle
 
 
-print(step_file)
+# Cross-section of a pipe
+def create_ifcarbitraryprofiledefwithvoids(ifcfile, area, area_name, outer_circle, inner_circle):
+    profilewithvoids = ifcfile.createIfcArbitraryProfileDefWithVoids(area, area_name, outer_circle, inner_circle)
+    return profilewithvoids
+
+
+# Axis 3D
+def create_ifcaxis2placement3D(ifcfile, point=O, dir1=Z, dir2=X):
+    point = ifcfile.createIfcCartesianPoint(point)
+    dir1 = ifcfile.createIfcDirection(dir1)
+    dir2 = ifcfile.createIfcDirection(dir2)
+    axis2placement3D = ifcfile.createIfcAxis2Placement3D(point, dir1, dir2)
+    return axis2placement3D
+
+# Axis 2D
+def create_ifcaxis2placement2D(ifcfile, point=O, dir2=X):
+    point = ifcfile.createIfcCartesianPoint(point)
+    dir2 = ifcfile.createIfcDirection(dir2)
+    axis2placement2D = ifcfile.createIfcAxis1Placement(point, dir2)
+    return axis2placement2D
+
+
+# Local placement (Location, Axis, RefDirection), specified as Python tuples, and relative placement
+def create_ifclocalplacement(ifcfile, point=O, dir1=Z, dir2=X, relative_to=None):
+    axis2placement3D = create_ifcaxis2placement3D(ifcfile, point, dir1, dir2)
+    ifclocalplacement = ifcfile.createIfcLocalPlacement(relative_to, axis2placement3D)
+    return ifclocalplacement
+
+
+# PolyLine from a list of points, specified as Python tuples
+def create_ifcpolyline(ifcfile, point_list):
+    ifcpts = []
+    for point in point_list:
+        point = ifcfile.createIfcCartesianPoint(point)
+        ifcpts.append(point)
+    polyline = ifcfile.createIfcPolyLine(ifcpts)
+    return polyline
+
+
+# Extrusion of an area in a specific direction
+def create_ifcextrudedareasolid(ifcfile, ifcclosedprofile, ifcaxis2placement, extrude_dir, extrusion):
+    ifcdir = ifcfile.createIfcDirection(extrude_dir)
+    ifcextrudedareasolid = ifcfile.createIfcExtrudedAreaSolid(ifcclosedprofile, ifcaxis2placement, ifcdir, extrusion)
+    return ifcextrudedareasolid
+
+
+# Definition of general file information
+filename = get_value(1, 0) + ".ifc"
+timestamp = time.time()
+timestring = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(timestamp))
+creator = "Alexander Wellenhofer"
+organization = "OTH Regensburg"
+application, application_version = "IfcOpenShell", "0.5"
+project_globalid, project_name = create_guid(), get_value(1, 0)
+
+
+# Definition of constant Elements which should be included in every IFC file
+template = """
+ISO-10303-21;
+HEADER;
+FILE_DESCRIPTION(('ViewDefinition [CoordinationView]'),'2;1');
+FILE_NAME('%(filename)s','%(timestring)s',('%(creator)s'),('%(organization)s'),'%(application)s','%(application)s','');
+FILE_SCHEMA(('IFC4'));
+ENDSEC;
+DATA;
+#1=IFCPERSON($,$,'%(creator)s',$,$,$,$,$);
+#2=IFCORGANIZATION($,'%(organization)s',$,$,$);
+#3=IFCPERSONANDORGANIZATION(#1,#2,$);
+#4=IFCAPPLICATION(#2,'%(application_version)s','%(application)s','');
+#5=IFCOWNERHISTORY(#3,#4,$,.ADDED.,$,#3,#4,%(timestamp)s);
+#6=IFCDIRECTION((1.,0.,0.));
+#7=IFCDIRECTION((0.,0.,1.));
+#8=IFCCARTESIANPOINT((0.,0.,0.));
+#9=IFCAXIS2PLACEMENT3D(#8,#7,#6);
+#10=IFCDIRECTION((0.,1.,0.));
+#11=IFCGEOMETRICREPRESENTATIONCONTEXT($,'Model',3,1.E-05,#9,#10);
+#12=IFCDIMENSIONALEXPONENTS(0,0,0,0,0,0,0);
+#13=IFCSIUNIT(*,.LENGTHUNIT.,$,.METRE.);
+#14=IFCSIUNIT(*,.AREAUNIT.,$,.SQUARE_METRE.);
+#15=IFCSIUNIT(*,.VOLUMEUNIT.,$,.CUBIC_METRE.);
+#16=IFCSIUNIT(*,.PLANEANGLEUNIT.,$,.RADIAN.);
+#17=IFCMEASUREWITHUNIT(IFCPLANEANGLEMEASURE(0.017453292519943295),#16);
+#18=IFCCONVERSIONBASEDUNIT(#12,.PLANEANGLEUNIT.,'DEGREE',#17);
+#19=IFCUNITASSIGNMENT((#13,#14,#15,#18));
+#20=IFCPROJECT('%(project_globalid)s',#5,'%(project_name)s',$,$,$,$,(#11),#19);
+ENDSEC;
+END-ISO-10303-21;
+""" % locals()  # Method, which updates and returns a dictionary
+
+
+# Write the elements to a temporary file
+temp_handle, temp_filename = tempfile.mkstemp(suffix=".ifc")
+with open(temp_filename, "w") as f:
+    f.write(template)
+
+
+# Obtain references to instances defined in template
+ifcfile = ifcopenshell.open(temp_filename)
+owner_history = ifcfile.by_type("IfcOwnerHistory")[0]
+project = ifcfile.by_type("IfcProject")[0]
+context = ifcfile.by_type("IfcGeometricRepresentationContext")[0]
+
+# IFC hierarchy creation (site, building, storey)
+site_placement = create_ifclocalplacement(ifcfile)
+site = ifcfile.createIfcSite(create_guid(), owner_history, "Site", None, None, site_placement, None, None, "ELEMENT",
+                             None, None, None, None, None)
+
+building_placement = create_ifclocalplacement(ifcfile, relative_to=site_placement)
+building = ifcfile.createIfcBuilding(create_guid(), owner_history, 'Building', None, None, building_placement, None,
+                                     None, "ELEMENT", None, None, None)
+
+storey_placement = create_ifclocalplacement(ifcfile, relative_to=building_placement)
+elevation = 0.0
+building_storey = ifcfile.createIfcBuildingStorey(create_guid(), owner_history, 'Storey', None, None, storey_placement,
+                                                  None, None, "ELEMENT", elevation)
+
+container_storey = ifcfile.createIfcRelAggregates(create_guid(), owner_history, "Building Container", None, building,
+                                                  [building_storey])
+container_site = ifcfile.createIfcRelAggregates(create_guid(), owner_history, "Site Container", None, site, [building])
+container_project = ifcfile.createIfcRelAggregates(create_guid(), owner_history, "Project Container", None, project,
+                                                   [site])
+
+
+#def create_ifclocalplacement(ifcfile, point=O, dir1=Z, dir2=X, relative_to=None):
+
+# Construction of geometry by extrusion of IfcArbitraryProfileDefWithVoids (two IfcCircles)
+pipe_placement = create_ifclocalplacement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), relative_to=storey_placement)  # Placement and direction of object
+polyline = create_ifcpolyline(ifcfile, [(0.0, 0.0, 0.0), (10.0, 0.0, 0.0)])  # Axis of object
+axis_representation = ifcfile.createIfcShapeRepresentation(context, "Axis", "Curve2D", [polyline])
+outer_circle = create_ifccircle(ifcfile, pipe_placement, 0.3)  # Outer circle
+inner_circle = create_ifccircle(ifcfile, pipe_placement, 0.25)  # Inner circle
+profile = create_ifcarbitraryprofiledefwithvoids(ifcfile, "AREA", "Pipe Profile", outer_circle,
+                                                 [inner_circle])  # Profile Definition
+extrusion_placement = create_ifcaxis2placement3D(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0),
+                                                 (1.0, 0.0, 0.0))  # Direction of extrusion
+solid = create_ifcextrudedareasolid(ifcfile, profile, extrusion_placement, (1.0, 0.0, 0.0), 10.0)  # Extrusion
+body_representation = ifcfile.createIfcShapeRepresentation(context, "Body", "SweptSolid", [solid])
+
+product_shape = ifcfile.createIfcProductDefinitionShape(None, None, [axis_representation, body_representation])
+pipe = ifcfile.createIfcPipeSegment(create_guid(), owner_history, "Pipe", "An awesome pipe", None, pipe_placement,
+                                    product_shape, None)
+
+
+# Define and associate the object material
+material = ifcfile.createIfcMaterial(get_value(1, 9))
+material_layer = ifcfile.createIfcMaterialLayer(material, 0.2, None)
+material_layer_set = ifcfile.createIfcMaterialLayerSet([material_layer], None)
+material_layer_set_usage = ifcfile.createIfcMaterialLayerSetUsage(material_layer_set, "AXIS2", "POSITIVE", -0.1)
+ifcfile.createIfcRelAssociatesMaterial(create_guid(), owner_history, RelatedObjects=[pipe],
+                                       RelatingMaterial=material_layer_set_usage)
+
+
+# Relate the object to the building storey
+ifcfile.createIfcRelContainedInSpatialStructure(create_guid(), owner_history, "Building Storey Container", None, [pipe],
+                                                building_storey)
+
+# Write the contents of the file to disk
+ifcfile.write(filename)
